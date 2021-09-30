80154b7cdd6185968c0369cf297a0cd05154c825
IT20273408 <it20273408@my.sliit.lk>
2021-09-30 17:13:12 +0530

    initial commit

---
+++ app/src/main/java/com/example/sqlitedemo/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.sqlitedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username,password,repassword;
+    Button singup,singin;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       username=findViewById(R.id.username);
+       password=findViewById(R.id.password);
+       singup=findViewById(R.id.signup);
+       singin=findViewById(R.id.signin);
+       DB= new DBHelper(this);
+
+       singup.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+              String user= username.getText().toString();
+              String pass= password.getText().toString();
+              String repass=repassword.getText().toString();
+
+              if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(repass))
+                  Toast.makeText(MainActivity.this, "All fields Required", Toast.LENGTH_SHORT).show();
+              else {
+                  if(pass.equals(repass)) {
+                      Boolean checkuser = DB.checkusername(user);
+                      if(checkuser==false) {
+                          Boolean insert = DB.insertData(user,pass);
+                          if (insert==true) {
+                              Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                              Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                              startActivity(intent);
+                          }else {
+                              Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+
+                          }
+
+                      } else {
+                          Toast.makeText(MainActivity.this, "User already Exists", Toast.LENGTH_SHORT ).show();
+
+                      }
+                  }else {
+                      Toast.makeText(MainActivity.this, "Passwords are not matching", Toast.LENGTH_SHORT).show();
+                  }
+
+              }
+           }
+       });
+
+       singin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+               startActivity(intent);
+
+           }
+       });
+
+
+    }
+}
